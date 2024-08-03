b687f5f39b79b3ea0bd8adab21fff27370cbb314
Kavan Patel <kavanpatel3421@gmail.com>
2024-08-03 22:36:38 +0530

    Conditionally run effects

--- state-hook/src/App.js
+++ state-hook/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import ClassCounter from './components/ClassCounter';
+import ClassCounterOne from './components/ClassCounterOne';
 import HookCounter from './components/HookCounter';
 import HookCounterFive from './components/HookCounterFive(effect)';
 import HookCounterFour from './components/HookCounterFour';
+import HookCounterOne from './components/HookCounterOne';
 import HookCounterThree from './components/HookCounterThree';
 import HookCounterTwo from './components/HookCounterTwo';
 
@@ -14,7 +16,9 @@ function App() {
       {/* <HookCounterTwo /> */}
       {/* <HookCounterThree /> */}
       {/* <HookCounterFour /> */}
-      <HookCounterFive />
+      {/* <HookCounterFive /> */}
+      {/* <ClassCounterOne /> */}
+      <HookCounterOne />
     </div>
   );
 }
